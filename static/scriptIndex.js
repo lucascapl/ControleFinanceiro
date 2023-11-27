2d8e9dee7693d0de64ae4440c078a73f584000f8
lucascapl <lucascapleal@gmail.com>
2023-11-27 15:26:53 -0300

    datepicker iniciando na data atual

--- static/scriptIndex.js
+++ static/scriptIndex.js
@@ -1,7 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Obter a data atual
+    var dataAtual = new Date();
+
+    var dia = ('0' + dataAtual.getDate()).slice(-2);
+    var mes = ('0' + (dataAtual.getMonth() + 1)).slice(-2);
+    var ano = dataAtual.getFullYear();
+    var dataFormatada = dia + '/' + mes + '/' + ano;
+
+    $('.datepickerElement').val(dataFormatada);
     // Inicialize o Datepicker
     $('.datepickerElement').datepicker({
         format: 'dd/mm/yyyy',  // Formato desejado da data
+        orientation: 'bottom',
         autoclose: true
     });
     var status = '{{ status }}';
